6171a0c608911d83f6e53f1e05807ad676c6c262
I Ngurah Gede Adi Putra <ngurahadi541@gmail.com>
2023-12-17 02:51:14 +0800

    Scene Beranda
    Perubahan UI dan tambahan Animasi

--- Assets/Script/AnimasiScene.cs
+++ Assets/Script/AnimasiScene.cs
@@ -5,45 +5,97 @@ using UnityEngine.SceneManagement;
 public class AnimasiScene : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-    [SerializeField] private string animasiAwal = "Tombol"; // Sesuaikan dengan nama animasi awal tombol Mulai
+    [SerializeField] private string animasiAwal = "Tombol";
     [SerializeField] private string animasiKlik = "Klik";
 
-    private void Awake()
+    private bool isAnimating = false;
+    private bool isFirstTime = true;
+    private bool exitButtonClicked = false; // Tambahkan variabel untuk menandakan apakah tombol keluar diklik
+
+    private void Start()
     {
-        // Dapatkan komponen Animator pada objek ini
         animator = GetComponent<Animator>();
-        
-        // Memulai animasi awal saat scene dimulai
+
         if (animator != null && !string.IsNullOrEmpty(animasiAwal))
         {
-            animator.Play(animasiAwal);
+            if (isFirstTime)
+            {
+                // Memainkan animasi awal hanya saat pertama kali memulai scene
+                animator.Play(animasiAwal);
+                isFirstTime = false;
+            }
         }
     }
 
     public void AnimasiDanPindah(string scene_name)
     {
-        // Memanggil fungsi animasi klik
-        AnimasiKlik();
+        if (!isAnimating)
+        {
+            exitButtonClicked = false; // Reset variabel exitButtonClicked
+            AnimasiKlik();
+            StartCoroutine(PindahSceneSetelahAnimasiSelesai(scene_name));
+        }
+    }
 
-        // Pindah scene setelah animasi klik selesai
-        StartCoroutine(PindahSceneSetelahAnimasiSelesai(scene_name));
+    public void KeluarAplikasi()
+    {
+        exitButtonClicked = true; // Set variabel exitButtonClicked menjadi true saat tombol keluar diklik
+        if (!isAnimating)
+        {
+            AnimasiKlik();
+            StartCoroutine(ExitApplicationAfterAnimation());
+        }
     }
 
     private void AnimasiKlik()
     {
-        // Memanggil fungsi animasi klik
         if (animator != null && !string.IsNullOrEmpty(animasiKlik))
         {
+            isAnimating = true;
             animator.Play(animasiKlik);
+            // Memanggil fungsi yang akan mengatur ulang animator setelah animasi selesai
+            StartCoroutine(ResetAnimasiSetelahSelesai());
         }
     }
 
     private IEnumerator PindahSceneSetelahAnimasiSelesai(string scene_name)
     {
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        // Setelah animasi selesai, pindah scene
+        SceneManager.LoadScene(scene_name);
+    }
+
+    private IEnumerator ResetAnimasiSetelahSelesai()
+    {
         // Menunggu durasi animasi selesai (sesuaikan dengan durasi animasi Anda)
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
 
-        // Pindah scene setelah animasi selesai
-        SceneManager.LoadScene(scene_name);
+        // Setelah animasi selesai, kembalikan ke animasi awal tanpa menjalankannya lagi
+        if (!string.IsNullOrEmpty(animasiAwal))
+        {
+            animator.Play(animasiAwal, 0, 1.0f); // Set progress ke akhir animasi
+        }
+
+        // Atur status animating kembali ke false
+        isAnimating = false;
+
+        // Jika tombol keluar diklik, keluar dari aplikasi setelah animasi tombol selesai
+        if (exitButtonClicked)
+        {
+            StartCoroutine(ExitApplicationAfterAnimation());
+        }
+    }
+
+    private IEnumerator ExitApplicationAfterAnimation()
+    {
+        // Menunggu durasi animasi selesai (sesuaikan dengan durasi animasi Anda)
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
--- Assets/Script/Musik.cs
+++ Assets/Script/Musik.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Musik : MonoBehaviour
@@ -7,47 +6,113 @@ public class Musik : MonoBehaviour
     private static Musik instance = null;
     private AudioSource audioSource;
 
-    [Range(0.0f, 1.0f)]
-    [SerializeField] private float initialVolume = 1.0f;
+    private bool isPlaying = false;
 
-    // Start is called before the first frame update
-    void Start()
+    private float normalVolume; // Volume normal musik utama
+    private bool isMusicPlaying = false; // Menandakan apakah musik utama sedang diputar
+
+    private float targetVolume; // Volume yang ingin dicapai
+    private Coroutine volumeChangeCoroutine;
+
+    void Awake()
     {
-        // Mengecek apakah objek instance sudah ada
-        if (instance != null)
+        if (instance == null)
         {
-            // Jika instance sudah ada, hancurkan objek yang baru
-            Destroy(gameObject);
+            instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
-            // Jika instance belum ada, jadikan objek ini instance
-            instance = this;
-
-            DontDestroyOnLoad(gameObject);
-
-            // Mendapatkan komponen AudioSource
-            audioSource = GetComponent<AudioSource>();
-
-            // Mengecek apakah audio sedang diputar
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
-
-            // Menetapkan nama objek
-            gameObject.name = "Musik";
-
-            // Mengatur volume dari PlayerPrefs atau nilai awal yang diatur di Inspector Unity
-            float savedVolume = PlayerPrefs.GetFloat("volume", initialVolume);
-            audioSource.volume = savedVolume;
+            Destroy(gameObject);
         }
     }
 
-    // Update is called once per frame
+    // Dipanggil sebelum frame pertama
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (PlayerPrefs.GetInt("isMusicPlaying", 0) == 1)
+        {
+            PlayPauseMusic();
+        }
+        else
+        {
+            PlayPauseMusic();
+        }
+
+        gameObject.name = "Musik";
+
+        normalVolume = audioSource.volume;
+        targetVolume = normalVolume;
+    }
+
+    // Dipanggil setiap frame
     void Update()
     {
-        // Memperbarui volume sesuai preferensi
-        audioSource.volume = PlayerPrefs.GetFloat("volume", initialVolume);
+        isMusicPlaying = audioSource.isPlaying;
+    }
+
+    // Fungsi untuk memainkan atau memberhentikan musik
+    public void PlayPauseMusic()
+    {
+        if (isPlaying)
+        {
+            audioSource.Pause();
+        }
+        else
+        {
+            audioSource.Play();
+        }
+
+        isPlaying = !isPlaying;
+
+        PlayerPrefs.SetInt("isMusicPlaying", isPlaying ? 1 : 0);
+    }
+
+    // Fungsi untuk mengatur volume ke nilai normal
+    public void NormalVolume()
+    {
+        if (!isMusicPlaying)
+        {
+            targetVolume = normalVolume;
+            StartCoroutine(ChangeVolumeOverTime(audioSource.volume, targetVolume));
+        }
+    }
+
+    // Fungsi untuk mengatur volume ke nilai yang lebih rendah (0.7f)
+    public void TurunVolume()
+    {
+        if (!isMusicPlaying)
+        {
+            targetVolume = 0.7f; // Atur nilai sesuai kebutuhan
+            StartCoroutine(ChangeVolumeOverTime(audioSource.volume, targetVolume));
+        }
+    }
+
+    // Coroutine untuk mengubah volume secara perlahan
+    private IEnumerator ChangeVolumeOverTime(float startVolume, float targetVolume, float duration = 1.0f)
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+            // Menghitung progress perubahan volume
+            float progress = elapsedTime / duration;
+
+            // Mengubah volume secara perlahan
+            audioSource.volume = Mathf.Lerp(startVolume, targetVolume, progress);
+
+            // Menunggu satu frame
+            yield return null;
+
+            elapsedTime += Time.deltaTime;
+        }
+
+        // Pastikan volume mencapai nilai target
+        audioSource.volume = targetVolume;
+
+        // Reset coroutine menjadi null untuk memungkinkan coroutine baru dimulai
+        volumeChangeCoroutine = null;
     }
 }
