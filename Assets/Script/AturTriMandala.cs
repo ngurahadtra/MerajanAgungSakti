625210e900e4312d49777acd720481e8409d72a7
I Ngurah Gede Adi Putra <ngurahadi541@gmail.com>
2024-01-09 11:05:29 +0800

    Fix Kodingan Ar Utama
    Minus belum rubah UI dan sound

--- Assets/Script/AturTriMandala.cs
+++ Assets/Script/AturTriMandala.cs
@@ -19,7 +19,21 @@ public class AturTriMandala : MonoBehaviour
 
     void Start()
     {
+        ID = 0;
+        SetAllPelinggihTags(); // Panggil metode ini untuk mengatur tag pada semua objek Pelinggih
         SpawnObject();
+    }
+
+    // Metode untuk mengatur tag pada semua objek Pelinggih
+    private void SetAllPelinggihTags()
+    {
+        foreach (GameObject pelinggihObj in Pelinggih)
+        {
+            if (pelinggihObj != null)
+            {
+                pelinggihObj.tag = "ObyekPelinggih";
+            }
+        }
     }
 
     public void SpawnObject()
@@ -41,13 +55,75 @@ public class AturTriMandala : MonoBehaviour
                 return;
             }
 
+            benda.tag = "ObyekPelinggih";
+
             benda.transform.SetParent(tempat.transform, false);
             benda.transform.localPosition = new Vector3(0, 0, 0);
-            benda.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+
+            // Pengecekan apakah objek adalah elemen nomor 27 atau 28
+            if (ID == 27 || ID == 28)
+            {
+                // Jika ya, atur rotasi sesuai kebutuhan
+                benda.transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                // Jika tidak, atur rotasi standar
+                benda.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+            }
+
+            // Hancurkan semua objek dengan tag "ObyekPelinggih" kecuali yang baru saja dibuat
+            GameObject[] BendaSebelumnyaArray = GameObject.FindGameObjectsWithTag("ObyekPelinggih");
+            foreach (GameObject bendaSebelumnya in BendaSebelumnyaArray)
+            {
+                if (bendaSebelumnya != benda)
+                {
+                    Destroy(bendaSebelumnya);
+                }
+            }
         }
         else
         {
             Debug.LogError("ID diluar rentang atau array TempatSpawn kosong.");
         }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            GantiPelinggih(true);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            GantiPelinggih(false);
+        }
+    }
+
+    public void GantiPelinggih(bool Kanan)
+    {
+        if (Kanan)
+        {
+            if (ID >= Pelinggih.Length - 1)
+            {
+                ID = 0;
+            }
+            else
+            {
+                ID++;
+            }
+        }
+        else
+        {
+            if (ID <= 0)
+            {
+                ID = Pelinggih.Length - 1;
+            }
+            else
+            {
+                ID--;
+            }
+        }
+        SpawnObject();
     }
 }
